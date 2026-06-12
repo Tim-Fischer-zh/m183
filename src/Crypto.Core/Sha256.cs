7986baa961120a146f2c3bfcabaa11eead0814cc
Tim-Fischer-zh <tim.elias.fischer.tef@gmail.com>
2026-06-12 10:53:57 +0200

    Konstanten für Motor

--- src/Crypto.Core/Sha256.cs
+++ src/Crypto.Core/Sha256.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace Crypto.Core;
 
@@ -17,7 +18,26 @@ public sealed class Sha256 : IHashFunction
      * ulong  // 64 Bit              
      */
     public int HashSizeInBytes => 32;
-
+    // Initiale Hash-Werte: Nachkomma-Bits der Quadratwurzeln der ersten 8 Primzahlen
+    private static readonly uint[] H0 =
+    {
+        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+        0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+    };
+    // Rundenkonstanten: Nachkomma-Bits der Kubikwurzeln der ersten 64 Primzahlen
+    private static readonly uint[] K =
+    {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
+    
+    
     public byte[] Hash(ReadOnlySpan<byte> data)
     {
         Pad(data.ToArray());        
@@ -31,7 +51,7 @@ public sealed class Sha256 : IHashFunction
         List<byte> block = new List<byte>(message); // mutable liste 
         ulong bitLen = (ulong)data.Length * 8; // erhaltene data länge, bei abc=24
         
-        block.Add(0x80); // ende der nachricht das hinzufügen also "abc" + 0x80 .....
+        block.Add(0x80); // Ende der nachricht das hinzufügen also "abc" + 0x80 .....
         
         for(int i = block.Count; i <56; i++) // bis index 56 mit 0 auffüllen
         {
@@ -49,18 +69,36 @@ public sealed class Sha256 : IHashFunction
         return block.ToArray();
     }
 
+    /*  a = 1100
+     *  b = 1010
+     *  a & b 1000 = 8 AND (beide 1)
+     *  a | b 1110 = 14 OR (mind. eins 1) 
+     *  a ^ b 0110 = 6 XOR (genau eins 1)
+     *  ~a 0011 = 3 NOT (alle Bits kippen)
+     */
+    
     private static uint RotR(uint x, int n)
     {
+        // shift 32bit uint um n, da aber shift Nullen einfüllt, müssen wir die verschobenen bits wieder am Ende einfügen
         return (x >> n) | (x << (32 - n));
     }
 
     private static uint Ch(uint e, uint f, uint g)
     {
+        /*
+         * e AND f XOR negiertesE AND g 
+         * (e)0011 AND (f)0101 = 0001 
+         * (~e)1100 AND (g)1110 = 1100
+         * (e&f)0001 XOR (~e&g)1100 = 1101
+         */ 
         return (e & f) ^ (~e & g);
     }
 
     private static uint Maj(uint a, uint b, uint c)
     {
+        /*
+         * (a AND b) XOR (a AND c) XOR (b AND c)
+         */
         return (a & b) ^ (a & c) ^ (b & c);
     }
 
