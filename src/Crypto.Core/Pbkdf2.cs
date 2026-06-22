c02de01329b7f5bdd28fb980cbda3af2509fadad
Tim-Fischer-zh <tim.elias.fischer.tef@gmail.com>
2026-06-22 23:23:47 +0200

    pbkdf2 iterationen gehen, methode public

--- src/Crypto.Core/Pbkdf2.cs
+++ src/Crypto.Core/Pbkdf2.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Crypto.Core;
 
@@ -6,12 +7,36 @@ public sealed class Pbkdf2 : IPasswordHasher
 {
     private readonly IMac _mac= new HmacSha256();
 
-    private byte[] Do(string password, byte[] salt, uint iterations, int dkLen)
+    public byte[] Do(string password, byte[] salt, uint iterations, int dkLen, uint index)
     {
+        byte[] bytePassword = Encoding.UTF8.GetBytes(password);
         // Salt || 00 00 00 01.
-        byte beIterations = (byte)(iterations >> 4);
-        salt[0] += beIterations;
-        return salt;
+        // index ist der block den der Hash ist. zb 2 oder 100
+        byte[] blockIndex = new byte[4];
+        blockIndex[0] = (byte)(index >> 24);
+        blockIndex[1] = (byte)(index >> 16);
+        blockIndex[2] = (byte)(index >> 8);
+        blockIndex[3] = (byte)(index);
+        
+        
+        var saltPlusCounter = salt.Concat(blockIndex).ToArray();
+        
+        var u1 = _mac.ComputeMac(bytePassword, saltPlusCounter);
+
+        byte[] u = u1;
+        byte[] t = (byte[])u1.Clone();
+
+        for (uint j = 2; j <= iterations; j++)
+        {
+            u = _mac.ComputeMac(bytePassword, u);
+            for (int k = 0; k < t.Length; k++)
+            {
+                // oder? t[k] ^= u[k];
+                t[k] = (byte)(t[k] ^ u[k]);
+            }
+        }
+        
+        return t;
     }
     
     public string Hash(string password)
@@ -20,8 +45,7 @@ public sealed class Pbkdf2 : IPasswordHasher
         byte[] salt = RandomNumberGenerator.GetBytes(16);
         uint iterations = 1;
         int dkLen = 32;
-        Do(password, salt, iterations, dkLen);
-        return BitConverter.ToString(Do(password, salt, iterations, dkLen));
+        return BitConverter.ToString(Do("password", salt, 1, 32, 1));
     }
 
     public bool Verify(string password, string storedHash)
--- src/Crypto.Lab/Program.cs
+++ src/Crypto.Lab/Program.cs
@@ -8,22 +8,21 @@ class Program
 {
     private readonly IHashFunction _hashFunction;
     private readonly IMac _mac;
+    private readonly IPasswordHasher _pbkdf2;
 
-    public Program(IHashFunction hashFunction, IMac mac)
+    public Program(IHashFunction hashFunction, IMac mac, IPasswordHasher pbkdf2)
     {
         _hashFunction = hashFunction;
         _mac = mac;
+        _pbkdf2 = pbkdf2;
     }
  
     static void Main()
     {
-        var p = new Program(new Sha256(), new HmacSha256());   
-        p.CallMac();
-        p.CallHash();
-        uint iterations = 0b0001;
-        Console.WriteLine($"Iterations : {iterations}");
-        byte beIterations = (byte)(iterations << 4);
-        Console.WriteLine($"Bei iteration : {beIterations}");
+        var p = new Program(new Sha256(), new HmacSha256(), new Pbkdf2());   
+        // p.CallMac();
+        // p.CallHash();
+        p.CallPbkdf2();
     }
 
     public void CallHash()
@@ -36,5 +35,13 @@ class Program
     {
         byte[] mac = _mac.ComputeMac(new byte[20], "Hi There"u8);
         Console.WriteLine(Convert.ToHexString(mac).ToLowerInvariant());
+    }
+
+    public void CallPbkdf2()
+    {
+        var sut = new Pbkdf2();
+        byte[] salt = System.Text.Encoding.ASCII.GetBytes("salt");
+        byte[] dk = sut.Do("password", salt, 4096, 32, 1);
+        Console.WriteLine(Convert.ToHexString(dk).ToLowerInvariant());
     }
 }
